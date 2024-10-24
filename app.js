ad51304d59b704a169a07a808dc7f1b86e1510aa
wenipsptsr <wenipuspitasari029@gmail.com>
2024-10-24 15:09:14 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+let tinggiBadan = [150, 155, 148, 165, 170, 168, 166];
+let namaAtlet = ['Rizki', 'Ambar', 'Rada', 'Tika', 'Malik', 'Nanang', 'Azizah'];
 
-
-
-
-
+for(let i = 0; i < tinggiBadan.length; i++) {
+  console.log("Atlet " + namaAtlet[i] + " tinggi badannya " + tinggiBadan[i] + "cm");
+};
+}
 
   /// JANGAN UBAH SELAINNYA
-}
 
 module.exports = runner
